fbf69eb6130862d3b0dbb51315095d1d5fcb5f18
sharger <sharger@sharger-glaptop.roam.corp.google.com>
2016-01-16 16:04:53 -0600

    Initial commit.

---
+++ public/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  function displayChatMessage(userId, text) {};
+
+  function setUser(id, name, photoUrl) {}
+
+  function login() {}
+
+  function sendMessage(text) {}
+
+  function setupViewHandlers() {}
+
+  function setupServerHandlers() {}
+
+  function init() {
+    setupViewHandlers();
+    setupServerHandlers();
+  }
+
+  init();
+})
