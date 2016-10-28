e4b51008367f11d2490d7582fe345438aa27aa3f
sharger <sharger@sharger-glaptop.roam.corp.google.com>
2016-10-27 22:42:26 -0700

    Completed implementation of basic Firebase site.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,59 @@
 $(document).ready(function() {
-  function displayChatMessage(userId, text) {};
+  var ENTER_KEY = 13;
+  var base = new Firebase("https://hackrice2015-sharger.firebaseio.com"); 
+  var tblMessages = base.child("messages");
 
-  function setUser(id, name, photoUrl) {}
+  var messageTemplate = _.template("<div><em><%= name %></em>:<%= text %></div>");
+  var currentUser = {"id": "guest", "name": "Guest", "profileImageUrl": "placeholder.jpg"};
 
-  function login() {}
+  function displayChatMessage(name, text) {
+    $('#messagesDiv').append(messageTemplate({"name": name, "text": text}));
+  };
 
-  function sendMessage(text) {}
+  function setUser(id, name, photoUrl) {
+    currentUser = {"id": id, "name": name, "photoUrl": photoUrl}
 
-  function setupViewHandlers() {}
+    $('.userImage').attr({src: photoUrl});
+  }
 
-  function setupServerHandlers() {}
+  function login() {
+    base.authWithOAuthPopup("google", function(error, authData) {
+      if (error) {
+        console.log("There was an error logging you in.");
+      } else {
+        setUser(
+            authData['google']['id'],
+            authData['google']['displayName'],
+            authData['google']['profileImageURL']);
+      }
+    }); 
+  }
+
+  function sendMessage(name, text) {
+    tblMessages.push({"name": name, "text": text})
+  }
+
+  function setupViewHandlers() {
+    $('#messageInput').keypress(function(e) {
+      if (e.keyCode == ENTER_KEY) {
+        var text = $('#messageInput').val(); 
+        sendMessage(currentUser['name'], text);
+        $('#messageInput').val('');
+      }
+    });
+
+    $('#loginButton').click(function(e) {
+      login();
+    }); 
+  }
+
+  function setupServerHandlers() {
+    tblMessages.on('child_added', function(snapshot) {
+      var message = snapshot.val();
+
+      displayChatMessage(message.name, message.text);
+    });
+  }
 
   function init() {
     setupViewHandlers();
